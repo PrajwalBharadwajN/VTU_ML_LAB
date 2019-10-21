4501a8d17527db53cb892a4e52cf877bba0a95ca
Prajwal Bharadwaj N <42998422+PrajwalBharadwajN@users.noreply.github.com>
2019-10-21 15:26:55 +0530

    Update knn.py

--- Machine Learning Lab/P9(KNN)/knn.py
+++ Machine Learning Lab/P9(KNN)/knn.py
@@ -2,18 +2,17 @@
 """
 @author: Prajwal Bharadwaj N
 """
-import pandas as pd
-df_irisbd = pd.read_csv("iris.txt",header=None,index_col=None)
-print(df_irisbd.head(20))
-X = df_irisbd.iloc[:, :-1].values
-y = df_irisbd.iloc[:, 4].values
+from sklearn import datasets
+iris = datasets.load_iris()
+
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8,test_size=0.2,random_state=100)
-print(y_test)
+X_train, X_test, y_train, y_test = train_test_split(iris.data,iris.target,train_size=0.8,test_size=0.2,random_state=100)
+
 from sklearn.neighbors import KNeighborsClassifier
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train,y_train)
 predicted= model.predict(X_test) 
+
 from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test, predicted))
 print(classification_report(y_test, predicted))
