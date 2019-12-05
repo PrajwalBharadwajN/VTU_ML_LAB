b3f8346547d4613930d5146c06c5878cc708359d
Prajwal Bharadwaj N <42998422+PrajwalBharadwajN@users.noreply.github.com>
2019-12-05 16:57:58 +0530

    Add files via upload

---
+++ Machine Learning Lab/P5(NBC)/NEW(shorterVersion)/naiveBayes.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 16:02:16 2019
+
+@author: Prajwal Bharadwaj N
+"""
+from functools import reduce
+import pprint
+import pandas as pd
+
+def calculate_priori():
+        class_values = list(set(data[class_attr]))
+        for i in class_values:
+            priori[i]  = class_data.count(i)/float(len(class_data))
+        print ("Priori Values: ", priori)
+   
+def get_cp(attr, attr_type, class_value):
+        data_attr = list(data[attr])
+        total =1
+        for i in range(0, len(data_attr)):
+            if class_data[i] == class_value and data_attr[i] == attr_type:
+                total+=1
+        return total/float(class_data.count(class_value))
+
+def calculate_conditional_probabilities(hypothesis):
+    for i in priori:
+        cp[i] = {}
+        for j in hypothesis:
+                cp[i].update({ hypothesis[j]: get_cp(j, hypothesis[j], i)})
+    print ("\nCalculated Conditional Probabilities: \n")
+    pprint.pprint(cp)
+
+def classify():
+        print ("Result: ")
+        for i in cp:
+            print (i, " ==> ", reduce(lambda x, y: x*y, cp[i].values())*priori[i])
+
+data = pd.read_csv("tennis.csv")
+print(data)
+class_attr = 'PlayTennis'
+print("Class Attribute:", class_attr)
+class_data =  list(data[class_attr])
+priori,cp={},{}
+calculate_priori()
+hypothesis = {"Outlook":'Sunny', "Temperature":"Mild", "Humidity":'Normal' , "Windy":'False'}
+print(hypothesis)
+calculate_conditional_probabilities(hypothesis)
+classify()
+print("-------------------------------------------------------")
+hypothesis = {"Outlook":'Overcast', "Temperature":"Cool", "Humidity":'High' , "Windy":'True'}
+print(hypothesis)
+calculate_conditional_probabilities(hypothesis)
+classify()
